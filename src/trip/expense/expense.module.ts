67c9e0466d6e26ddda182578f721fd5aa043b864
Bernice Asaah <berniceasamoahasaah@gmail.com>
2024-04-12 22:57:56 +0200

    Fix Getting Expenses for a Trip

--- src/trip/expense/expense.module.ts
+++ src/trip/expense/expense.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { Trip, TripSchema } from '../trip.schema';
 import { ExpenseController } from './expense.controller';
 import { Expense, ExpenseSchema } from './expense.schema';
 import { ExpenseService } from './expense.service';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Expense.name, schema: ExpenseSchema }]),
+    MongooseModule.forFeature([
+      { name: Expense.name, schema: ExpenseSchema },
+      { name: Trip.name, schema: TripSchema },
+    ]),
   ],
   controllers: [ExpenseController],
   providers: [ExpenseService],
--- src/trip/expense/expense.service.ts
+++ src/trip/expense/expense.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { Trip } from '../trip.schema';
 import { CreateExpenseDto } from './dto/create-expense.dto';
 import { UpdateExpenseDto } from './dto/update-expense.dto';
 import { Expense } from './expense.schema';
@@ -10,6 +11,8 @@ export class ExpenseService {
   constructor(
     @InjectModel(Expense.name)
     private expenseModel: Model<Expense>,
+    @InjectModel(Trip.name)
+    private tripModel: Model<Trip>,
   ) {}
 
   async createExpense(createExpenseDto: CreateExpenseDto): Promise<Expense> {
@@ -18,7 +21,8 @@ export class ExpenseService {
   }
 
   async findAllExpenses(tripId: string): Promise<Expense[]> {
-    return await this.expenseModel.find({ trip: tripId }).exec();
+    const trip = await this.tripModel.findById(tripId).exec();
+    return await this.expenseModel.find({ _id: { $in: trip.expenses } }).exec();
   }
 
   async findAll(): Promise<Expense[]> {
