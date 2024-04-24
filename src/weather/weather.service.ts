35ccd71a196e1ab2b459be548f046e7fbc764b44
Bernice Asaah <berniceasamoahasaah@gmail.com>
2024-04-24 21:02:42 +0200

    Error Handling in Weather Service

--- src/weather/weather.service.ts
+++ src/weather/weather.service.ts
@@ -4,15 +4,31 @@ import axios from 'axios';
 @Injectable()
 export class WeatherService {
   async getWeather(place: string) {
-    const response = await axios.get(`https://wttr.in/${place}?format=j1`);
-    const data = response.data;
+    try {
+      const response = await axios.get(`https://wttr.in/${place}?format=j1`);
+      const data = response.data;
 
-    const currentTemp = data.current_condition[0].temp_C;
-    const next2DaysTemp = data.weather.slice(1, 3).map((day) => day.avgtempC);
+      if (data.current_condition && data.weather) {
+        const currentTemp = data.current_condition[0]?.temp_C || null;
+        const next2DaysTemp = data.weather
+          .slice(1, 3)
+          .map((day) => day.avgtempC);
 
-    return {
-      currentTemp,
-      next2DaysTemp,
-    };
+        return {
+          currentTemp,
+          next2DaysTemp,
+        };
+      } else {
+        return {
+          currentTemp: null,
+          next2DaysTemp: [],
+        };
+      }
+    } catch (error) {
+      return {
+        currentTemp: null,
+        next2DaysTemp: [],
+      };
+    }
   }
 }
